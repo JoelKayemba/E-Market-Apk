5a6c7bdad1ba68152638e777e8fa94f5f72e3efd
kayembajoel92@gmail.com <kayembajoel92@gmail.com>
2024-08-04 16:41:43 -0400

    finition

--- CompteClient/Screen/PageMonProduit.js
+++ CompteClient/Screen/PageMonProduit.js
@@ -268,7 +268,7 @@ const styles = StyleSheet.create({
     sizeBox: {
         padding: 10,
         borderWidth: 1,
-        borderColor: Color.orange,
+        borderColor: Color.bleu,
         borderRadius: 5,
         margin: 5,
     },
